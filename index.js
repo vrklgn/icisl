00eed0a323c00e21ec2c9ffc64a4c50cb748e65f
Robert Levelind <robert.levelind@tink.com>
2024-08-27 16:34:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+// START GENAI@CHATGPT4
 var express = require('express');
+var axios = require('axios');
 var app = express();
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
+
+const fetchXMLFeed = async () => {
+    const url = "https://share.garmin.com/Feed/Share/kebnekaise";
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
+
+app.get('/fetch-feed', async (req, res) => {
+    try {
+        const data = await fetchXMLFeed();
+
+        // Send the data as the response
+        res.send(data);
+    } catch (error) {
+        res.status(500).send(error.toString());
+    }
+});
 
 app.listen(8080, function(){
     console.log('App is listening on port 8080');
