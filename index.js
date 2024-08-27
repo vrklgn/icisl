d2f3dcb16e8adc5712ba247a3acb7b411b6970a7
Robert Levelind <robert.levelind@tink.com>
2024-08-27 16:25:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var app = express();
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-    console.log('App is listening on port 3000');
+app.listen(8080, function(){
+    console.log('App is listening on port 8080');
 });
