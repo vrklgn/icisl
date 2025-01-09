6807d3df08190148ce711c947c86263672aa031c
Robert Levelind <robert.levelind@tink.com>
2025-01-09 16:01:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = 8080; // Set the server to listen on port 8080
